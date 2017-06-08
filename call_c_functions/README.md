1411c962de23385575c432b1e75ae7d28f58955a
Mattia Roccoberton <mroccoberton@mbpdev004.local>
2017-06-08 07:52:27 +0200

    Improvements

---
+++ call_c_functions/README.md
@@ -0,0 +1,5 @@
+# Call C functions
+
+Some examples which call functions from external C libs.
+
+_ffi_ gem is required
---
+++ call_c_functions/test2.rb
@@ -0,0 +1,10 @@
+require 'ffi'
+
+module FfiMathTest
+  extend FFI::Library
+  ffi_lib 'c'
+  ffi_lib 'm'
+  attach_function :pow, [ :double, :double ], :double
+end
+
+puts FfiMathTest.pow( 2, 3 )
