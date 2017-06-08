ed89ec6e17b01707b29f4b16ccbffee3bb1685af
Mattia Roccoberton <mroccoberton@mbpdev004.local>
2017-06-08 22:20:11 +0200

    rack_basic_app

--- rack_basic_app/README.md
+++ rack_basic_app/README.md
@@ -1,3 +1,7 @@
 # Rack Basic App
 
-A very simple sample app using Rack :)
+A very simple sample app using Rack - Harder, Better, Faster, Stronger :)
+
+### run
+
+`rackup -p 3000`
--- rack_basic_app/app/test.rb
+++ rack_basic_app/app/test.rb
@@ -1,15 +1,11 @@
 class Test
-
-    def initialize()
+  def call( env )
+    req = Rack::Request.new( env )
+    case env['REQUEST_PATH']
+    when '/'
+      [ 200, { 'Content-Type' => 'text/html' }, [ 'Home!' ] ]
+    else
+      [ 404, { 'Content-Type' => 'text/html' }, [ 'This is not the page you are looking for...' ] ]
     end
-
-    def call( env )
-        req = Rack::Request.new( env )
-        case env['REQUEST_PATH']
-        when '/'
-            [ 200, { 'Content-Type' => 'text/html' }, [ 'Home!' ] ]
-        else
-            [ 404, { 'Content-Type' => 'text/html' }, [ 'This is not the page you are looking for...' ] ]
-        end
-    end
+  end
 end
